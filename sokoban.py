af26b1d493d40d17b8642e5d76a35862d4b9b744
dzejkop2 <91260006+dzejkop2@users.noreply.github.com>
2022-02-04 11:20:05 +0100

    Update sokoban.py
    všetko je stratene

--- sokoban.py
+++ sokoban.py
@@ -1,97 +1,63 @@
 import pyglet
-import random
+from pyglet import image
 
 from pyglet.window import key
-from pyglet import gl
 
 WIDTH = 1000
 HEIGHT = 800
 
-SPEED = 200
+size = 20
+robko_x = WIDTH // size // 2 * size
+robko_y = HEIGHT // size // 2 * size
 
-rychlost_robka = [0,0]
-VELKOST_ROBKA = 20
-pozicia_robka = [WIDTH // 2, HEIGHT // 2]
+robko_mx = 0
+robko_my = 0
 
-klavesy = set()
+pressed_keys = set()
 
 window = pyglet.window.Window(width=WIDTH, height=HEIGHT)
 
-"""
-def text(text, x, y):
-    text = pyglet.text.Label(text,font_size=36,x=x,y=y,anchor_x="center")
-    text.draw()
+@window.event
+def on_draw():
+    window.clear()
+    draw_square(robko_x,robko_y,size)
 
-def draw():
-    text("Sokoban :)", x=WIDTH /2, y=HEIGHT/2)
-"""
-
-def square(x1,y1,x2,y2):
-    gl.glBegin(gl.GL_TRIANGLE_FAN)
-    gl.glVertex2f(int(x1), int(y1))
-    gl.glVertex2f(int(x1), int(y2))
-    gl.glVertex2f(int(x2), int(y2))
-    gl.glVertex2f(int(x2), int(y1))
-    gl.glEnd()
-
-def reset():
-    pozicia_robka[0] = WIDTH // 2
-    pozicia_robka[1] = HEIGHT // 2
-
-    if random.randint(0, 1):
-        rychlost_robka[0] = SPEED
-    else:
-        rychlost_robka[0] = -SPEED
-
-    rychlost_robka[1] = random.uniform(-1, 1) * SPEED
-
-def hrac():
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-    gl.glColor3f(1, 1, 1)
-    square(
-        pozicia_robka[0] - VELKOST_ROBKA // 2,
-        pozicia_robka[1] - VELKOST_ROBKA // 2,
-        pozicia_robka[0] + VELKOST_ROBKA // 2,
-        pozicia_robka[1] + VELKOST_ROBKA // 2
-    )
+def draw_square(x, y, size, colour = (255,255,255,0)):
+    img = image.create(size, size, image.SolidColorImagePattern(colour))
+    img.blit(x,y)
 
 
-#pridava stlacene klavesi
-def key_press(symbol,modifier):
-    if symbol == key.UP:
-        klavesy.add('UP')
-    if symbol == key.DOWN:
-        klavesy.add('DOWN')
-    if symbol == key.LEFT:
-        klavesy.add('LEFT')
-    if symbol == key.RIGHT:
-        klavesy.add('RIGHT')
+def on_key_press(symbol,modifier):
+    if symbol == key.W:
+        pressed_keys.add("UP")
+    if symbol == key.S:
+        pressed_keys.add("DOWN")
 
-#odobera pustene klavesi
-def key_release(symbol,modifier):
-    if symbol == key.UP:
-        klavesy.discard('UP')
-    if symbol == key.DOWN:
-        klavesy.discard('DOWN')
-    if symbol == key.LEFT:
-        klavesy.discard('LEFT')
-    if symbol == key.RIGHT:
-        klavesy.discard('RIGHT')
+def on_key_release(symbol,modifier):
+    if symbol == key.W:
+        pressed_keys.discard("UP")
+    if symbol == key.S:
+        pressed_keys.discard("DOWN")
 
-def posuvanie(dt):
-    if ('UP') in klavesy:
-        pozicia_robka[0] += rychlost_robka * dt
-    if ('DOWN') in klavesy:
-        pozicia_robka[1] -= rychlost_robka * dt
+def move():
+    global robko_mx, robko_my
+    if ("UP") in pressed_keys:
+        robko_mx = 0
+        robko_my = size
+    if ("DOWN") in pressed_keys:
+        robko_mx = 0
+        robko_my = -size
 
 
-reset()
+def update(dt):
+    global robko_x,robko_y
+    robko_x += robko_mx
+    robko_y += robko_my
+
+
 window.push_handlers(
-    on_draw=hrac,
-    on_key_press=key_press,
-    on_key_release=key_release
+    on_key_press=on_key_press,
+    on_key_release=on_key_release
 )
-
-pyglet.clock.schedule(posuvanie)
-
+pyglet.clock.schedule_interval(update,1/30)
 pyglet.app.run()
