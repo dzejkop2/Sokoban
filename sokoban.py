64dc07fd2cf4950bfe9e8e963a97931929d426fc
dzejkop2 <91260006+dzejkop2@users.noreply.github.com>
2022-02-10 14:30:14 +0100

    skušam odstraniť bug
    robko sa objavil

--- sokoban.py
+++ sokoban.py
@@ -1,103 +1,114 @@
 import pyglet
-import math
 from pyglet import image
 from pyglet.window import key
 
 WIDTH = 1200
-HEIGHT = 1000
+HEIGHT = 800
 
 size = 40
 size_box = 40
-size_barrier = 40
 
 robko_x = WIDTH // size // 2 * size
 robko_y = HEIGHT // size // 2 * size
 
-box_x = WIDTH // size_box // 3 * size_box
-box_y = WIDTH // size_box // 3 * size_box
+box1_x = 400
+box1_y = 400
 
-box_mx = 0
-box_my = 0
+box1_mx = 0
+box1_my = 0
 
 robko_mx = 0
 robko_my = 0
 
 window = pyglet.window.Window(width=WIDTH, height=HEIGHT)
 
+#vykresluje kocky
 @window.event
 def on_draw():
     window.clear()
-    draw_square(robko_x,robko_y,size, (255,255,255,0))
-    draw_square(box_x, box_y, size_box, (255,1,1,0))
+    draw_square(robko_x,robko_y,size, (255,255,255,0) )
+    draw_square(box1_x, box1_y, size_box,(255,1,1,0))
 
-
-def draw_square(x, y, size, colour):
-    img = image.create(size, size, image.SolidColorImagePattern(colour))
+#zadáva ako vykresliť kocku
+def draw_square(x, y, size, color):
+    img = image.create(size, size, image.SolidColorImagePattern(color))
     img.blit(x,y)
 
-def barrier():
-    if
+#zadáva kde sa môže pohybovať
+def barrier(dt):
+    global robko_mx, robko_my, box1_mx, box1_my
+    if robko_x + size == WIDTH:
+        robko_mx = 0
+    if robko_x == 0:
+        robko_mx = 0
+    if robko_y + size == HEIGHT:
+        robko_my = 0
+    if robko_y == 0:
+        robko_my = 0
 
+#movement
 def on_key_press(symbol,modifier):
-    global robko_mx, robko_my,box_mx,box_my,move_box
+    global robko_mx, robko_my,box1_mx,box1_my,move_box
     if symbol == key.W:
         robko_mx = 0
         robko_my = size
-        if robko_x == box_x:
-            if robko_y + size == box_y:
-                box_my = size_box
+        if robko_x == box1_x:
+            if robko_y + size == box1_y:
+                box1_my = size_box
     if symbol == key.S:
         robko_mx = 0
         robko_my = -size
-        if robko_x == box_x:
-            if robko_y - size == box_y:
-                box_my = -size_box
+        if robko_x == box1_x:
+            if robko_y - size == box1_y:
+                box1_my = -size_box
     if symbol == key.A:
         robko_mx = -size
         robko_my = 0
-        if robko_y == box_y:
-            if robko_x - size == box_x:
-                box_mx = -size_box
+        if robko_y == box1_y:
+            if robko_x - size == box1_x:
+                box1_mx = -size_box
     if symbol == key.D:
         robko_mx = size
         robko_my = 0
-        if robko_y == box_y:
-            if robko_x + size == box_x:
-                box_mx = size_box
+        if robko_y == box1_y:
+            if robko_x + size == box1_x:
+                box1_mx = size_box
+
+
 def on_key_release(symbol,modifier):
-    global robko_mx, robko_my,box_mx,box_my,move_box
+    global robko_mx, robko_my,box1_mx,box1_my,move_box
     if symbol == key.W:
         robko_mx = 0
         robko_my = 0
-        box_mx = 0
-        box_my = 0
+        box1_mx = 0
+        box1_my = 0
     if symbol == key.S:
         robko_mx = 0
         robko_my = 0
-        box_mx = 0
-        box_my = 0
+        box1_mx = 0
+        box1_my = 0
     if symbol == key.A:
         robko_mx = 0
         robko_my = 0
-        box_mx = 0
-        box_my = 0
+        box1_mx = 0
+        box1_my = 0
     if symbol == key.D:
         move_box = 0
         robko_mx = 0
         robko_my = 0
-        box_mx = 0
-        box_my = 0
+        box1_mx = 0
+        box1_my = 0
 
 
 def update(dt):
-    global robko_x,robko_y,box_x,box_y,box_mx,box_my
+    global robko_x,robko_y,box1_x,box1_y,box1_mx,box1_my
     robko_x += robko_mx
     robko_y += robko_my
-    box_x += box_mx
-    box_y += box_my
+    box1_x += box1_mx
+    box1_y += box1_my
 
 pyglet.clock.schedule_interval(update, 1/15)
-
+pyglet.clock.schedule_interval(barrier, 1/15)
 window.push_handlers(
     on_key_press=on_key_press,
     on_key_release=on_key_release
